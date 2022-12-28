fc9d604333bf1fe8321f98d11f58cb132c920af9
Izaak Kuipers <izaak.kuipers@gmail.com>
2022-12-28 20:36:15 +0100

    Add username to settings sidebar

--- src/lib/Apps/Settings/Sidebar.svelte
+++ src/lib/Apps/Settings/Sidebar.svelte
@@ -4,20 +4,37 @@
     setSettingsPage,
     SettingsPages,
   } from "../../../ts/applogic/apps/SettingsApp/main";
+  import { UserData, UserName } from "../../../ts/userlogic/interfaces";
+  import { getProfilePicture } from "../../../ts/userlogic/pfp";
+
+  let pfp = "";
+
+  UserData.subscribe(() => {
+    pfp = getProfilePicture(parseInt($UserData.acc.profilePicture as string));
+  });
 </script>
 
 <div class="sidebar">
-  {#each SettingsPages as page}
-    <button
-      class="page"
-      class:selected={$currentSettingsPage &&
-        page.name == $currentSettingsPage.name}
-      on:click={() => setSettingsPage(page)}
-      disabled={page.disabled}
-    >
-      <img src={page.icon} alt={page.name} class="icon" />
-      <span class="caption">{page.name}</span>
-    </button>
-    {#if page.sep}<hr />{/if}
-  {/each}
+  <div class="pages">
+    {#each SettingsPages as page}
+      <button
+        class="page"
+        class:selected={$currentSettingsPage &&
+          page.name == $currentSettingsPage.name}
+        on:click={() => setSettingsPage(page)}
+        disabled={page.disabled}
+      >
+        <img src={page.icon} alt={page.name} class="icon" />
+        <span class="caption">{page.name}</span>
+      </button>
+      {#if page.sep}<hr />{/if}
+    {/each}
+  </div>
+  <div class="userprofile">
+    <img src={pfp} alt={$UserName} class="pfp" />
+    <div class="username">
+      <p class="name">{$UserName}</p>
+      <p class="hostname">{location.hostname}</p>
+    </div>
+  </div>
 </div>
