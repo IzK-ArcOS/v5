b1dd627b8a6a7e9ec3c56188e55f5dacbd706a7b
Izaak Kuipers <izaak.kuipers@gmail.com>
2022-12-31 15:23:02 +0100

    Fix error in action center notification renderer

--- src/lib/Page/Desktop/ActionCenter/Notifications.svelte
+++ src/lib/Page/Desktop/ActionCenter/Notifications.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { NotificationStore } from "../../../../ts/notiflogic/main";
+  import Notification from "./Notifications/Notification.svelte";
 </script>
 
 <div class="notifications">
-  {#each Object.entries($NotificationStore) as notification}{/each}
+  {#each Object.entries($NotificationStore) as notification}
+    <Notification {notification} />
+  {/each}
 </div>
