8003244d8fece15e20bfe13ffc669760c2fad7f7
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-01-18 23:59:43 +0100

    add kb shortcuts for calculator

--- src/ts/applogic/apps/Calculator.ts
+++ src/ts/applogic/apps/Calculator.ts
@@ -1,6 +1,7 @@
 import logo from "../../../assets/apps/calculator.svg";
 import Calculator from "../../../lib/Apps/Calculator.svelte";
 import type { App } from "../interface";
+import { CalculatorClass, CalculatorValue } from "./Calculator/main";
 
 export const CalculatorApp: App = {
   info: {
@@ -23,4 +24,29 @@ export const CalculatorApp: App = {
   },
   content: Calculator,
   glass: true,
+  events: {
+    close() {
+      CalculatorValue.set("");
+    },
+    keyboardShortcuts: [
+      { key: "0", action: () => CalculatorClass.processKey("0") },
+      { key: "1", action: () => CalculatorClass.processKey("1") },
+      { key: "2", action: () => CalculatorClass.processKey("2") },
+      { key: "3", action: () => CalculatorClass.processKey("3") },
+      { key: "4", action: () => CalculatorClass.processKey("4") },
+      { key: "5", action: () => CalculatorClass.processKey("5") },
+      { key: "6", action: () => CalculatorClass.processKey("6") },
+      { key: "7", action: () => CalculatorClass.processKey("7") },
+      { key: "8", action: () => CalculatorClass.processKey("8") },
+      { key: "9", action: () => CalculatorClass.processKey("9") },
+      { key: "0", action: () => CalculatorClass.processKey("0") },
+      { key: "/", action: () => CalculatorClass.processKey("/") },
+      { key: "+", action: () => CalculatorClass.processKey("+") },
+      { key: "-", action: () => CalculatorClass.processKey("-") },
+      { key: "*", action: () => CalculatorClass.processKey("*") },
+      { key: "^", action: () => CalculatorClass.processKey("^") },
+      { key: "enter", action: () => CalculatorClass.evaluate() },
+      { key: "c", action: () => CalculatorValue.set("") },
+    ],
+  },
 };
