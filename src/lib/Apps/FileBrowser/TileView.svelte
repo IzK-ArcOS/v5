6f28dce5aed3297387dacad9f9eb32979c7d7d4b
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-02-09 09:33:46 +0100

    Fix light ArcFind, improve FileManager tile view

--- src/lib/Apps/FileBrowser/TileView.svelte
+++ src/lib/Apps/FileBrowser/TileView.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import "../../../css/desktop/apps/filebrowser/tile.css";
   import type { PartialUserDir, UserFile } from "../../../ts/api/interface";
+  import { FileBrowserRefreshing } from "../../../ts/applogic/apps/FileBrowser/main";
+  import Spinner from "../../Spinner.svelte";
   import Dir from "./TileView/Dir.svelte";
   import File from "./TileView/File.svelte";
 
@@ -11,19 +13,32 @@
   dirs;
 </script>
 
-<div class="tileview">
-  {#if dirs.length}
-    <div class="folders">
-      {#each dirs as dir}
-        <Dir {dir} />
-      {/each}
+{#if !dirs.length && !files.length}
+  {#if $FileBrowserRefreshing}
+    <div class="loading">
+      <div class="loading-inner">
+        <Spinner height={32} />
+        <p class="caption">Reading items...</p>
+      </div>
     </div>
+  {:else}
+    <div class="nofiles">This folder is empty.</div>
   {/if}
-  {#if files.length}
-    <div class="files">
-      {#each files as file}
-        <File {file} />
-      {/each}
-    </div>
-  {/if}
-</div>
+{:else}
+  <div class="tileview">
+    {#if dirs.length}
+      <div class="folders">
+        {#each dirs as dir}
+          <Dir {dir} />
+        {/each}
+      </div>
+    {/if}
+    {#if files.length}
+      <div class="files">
+        {#each files as file}
+          <File {file} />
+        {/each}
+      </div>
+    {/if}
+  </div>
+{/if}
