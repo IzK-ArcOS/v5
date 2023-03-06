2891e58af6e589d2728f2b09fd4dd4773b7fa22e
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-03-06 20:54:03 +0100

    Improve img0 wallpaper selector

--- src/lib/Apps/Settings/Pages/Desktop/ImageSelector.svelte
+++ src/lib/Apps/Settings/Pages/Desktop/ImageSelector.svelte
@@ -12,7 +12,11 @@
 <div
   class="wallpaper"
   style="background-image: url({wallpaper[1]});"
-  title={wallpaper[0]}
+  title={wallpaper[0] == "img0" ? "(none)" : wallpaper[0]}
   class:selected={$UserData.sh.desktop.wallpaper == wallpaper[0]}
   on:click={change}
-/>
+>
+  {#if wallpaper[0] == "img0"}
+    <span class="material-icons-round none">block</span>
+  {/if}
+</div>
