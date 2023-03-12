26a6b626bb0139e6ac6f5d60aadde043058f75cf
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-03-12 20:18:53 +0100

    Remove custom apps from start menu applist

--- src/lib/Page/Desktop/StartMenu/LeftPane.svelte
+++ src/lib/Page/Desktop/StartMenu/LeftPane.svelte
@@ -5,7 +5,7 @@
 
 <div class="left">
   {#each $WindowStore as app}
-    {#if !app.disabled && !app.info.hidden}
+    {#if !app.disabled && !app.info.hidden && !app.info.custom}
       <AppListItem {app} />
     {/if}
   {/each}
