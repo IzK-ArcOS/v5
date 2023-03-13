c57b436a805d7e37e7d591132338ce41f5ea1b13
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-03-13 21:19:16 +0100

    Update AppMan bottom bar

--- src/lib/Apps/AppManager/BottomBar.svelte
+++ src/lib/Apps/AppManager/BottomBar.svelte
@@ -1,10 +1,20 @@
 <script>
-  import { OpenedStore, WindowStore } from "../../../ts/applogic/store";
+  import { appManSelected } from "../../../ts/applogic/apps/AppManager/store";
+  import {
+    getWindow,
+    OpenedStore,
+    WindowStore,
+  } from "../../../ts/applogic/store";
   import { ErrorWindowStore } from "../../../ts/errorlogic/app";
 </script>
 
 <div class="bottombar">
   <div class="right">
+    {#if $appManSelected}
+      <div class="section">
+        Snapped: {getWindow($appManSelected).snapped ? "yes" : "no"}
+      </div>
+    {/if}
     <div class="section">
       Running: {$OpenedStore.length}
     </div>
