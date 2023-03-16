ed692f6c3652b5af6ac14f3df236cba9e47aab5e
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-03-16 15:49:11 +0100

    Update about page

--- src/lib/Apps/Settings/Pages/About.svelte
+++ src/lib/Apps/Settings/Pages/About.svelte
@@ -27,22 +27,15 @@
 
 <div class="about-page">
   <div class="product">
-    <div class="about">
-      Thank you for using ArcOS. This project aims to reach the limits of
-      creating an OS-like environment in a browser. This project was built using
-      vanilla Javascript, and later rewritten in Svelte-TS.
-    </div>
     <div><img src={logo} alt="ArcOS" class="logo" /></div>
     <div class="text">
       <h1 class="name">ArcOS</h1>
     </div>
   </div>
   <Section header="License">
-    The whole ArcOS project is licensed under
-    <a href="https://www.gnu.org/licenses/gpl-3.0.txt" target="_blank">
-      GPLv3</a
-    >.<br /><br />More information about the license and the project as a whole<br
-    />
+    The whole ArcOS project is licensed under GPLv3.<br /><br />
+    More information about the license and the project as a whole
+    <br />
     can be found in the README file of the
     <a href="https://github.com/IzK-ArcOS">ArcOS project page</a>.
   </Section>
@@ -51,21 +44,23 @@
     scenes of sites you visit? Developer mode is a tool we use internally<br
     />to monitor different states in ArcOS. You can choose to enable it<br />
     <b>For this user only</b>.
-    <button
-      class="action martop"
-      on:click={enableDevmode}
-      disabled={$UserData.devmode}
-    >
-      <span class="material-icons-round">bug_report</span>
-      Enable developer mode
-    </button>
-    <button
-      class="action martop"
-      on:click={disableDevmode}
-      disabled={!$UserData.devmode}
-    >
-      <span class="material-icons-round">power_settings_new</span>
-      Disable developer mode
-    </button>
+    <div class="devmode">
+      <button
+        class="devmode martop"
+        on:click={enableDevmode}
+        disabled={$UserData.devmode}
+      >
+        <span class="material-icons-round">bug_report</span>
+        Enable devmode
+      </button>
+      <button
+        class="devmode martop"
+        on:click={disableDevmode}
+        disabled={!$UserData.devmode}
+      >
+        <span class="material-icons-round">power_settings_new</span>
+        Disable devmode
+      </button>
+    </div>
   </Section>
 </div>
