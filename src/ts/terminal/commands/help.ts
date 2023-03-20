45d600f90e6d846faf86104aa5b4a19c97cceef8
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-03-20 17:35:38 +0100

    Update ChooserOverlay, ArcTerm: `help`

--- src/ts/terminal/commands/help.ts
+++ src/ts/terminal/commands/help.ts
@@ -3,8 +3,6 @@ import type { Command } from "../interface";
 export const Help: Command = {
   keyword: "help",
   exec(cmd, argv, term) {
-    term.util.writeLine("\nThese are the built-in commands:\n");
-
     for (let i = 0; i < term.commands.length; i++) {
       const a = term.commands[i].keyword.toUpperCase().padEnd(15, " ");
       const b = term.commands[i].description;
