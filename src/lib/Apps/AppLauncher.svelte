b7065206007deb923ad61175b5febb3a9bbd13a4
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-06-27 18:23:40 +0200

    Continue AppLauncher

---
+++ src/lib/Apps/AppLauncher.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { isPopulatable } from "../../ts/applogic/checks";
+  import { WindowStore } from "../../ts/applogic/store";
+  import AppListItem from "../Page/Desktop/StartMenu/AppListItem.svelte";
+</script>
+
+<div class="apps">
+  {#each $WindowStore as app}
+    {#if isPopulatable(app)}
+      <AppListItem {app} />
+    {/if}
+  {/each}
+</div>
