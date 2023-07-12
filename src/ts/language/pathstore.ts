fa171f2242bf2ee30d36bce34d359bf588045aa5
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-07-12 13:44:12 +0200

    Update Language paths

--- src/ts/language/pathstore.ts
+++ src/ts/language/pathstore.ts
@@ -3,10 +3,10 @@ import type { LanguagePathStore } from "./interface";
 export const langPathStore: LanguagePathStore = {
   "en-US": {
     name: "English",
-    path: "/lang/en-US.json",
+    path: "./lang/en-US.json",
   },
   "nl-NL": {
     name: "Nederlands",
-    path: "/lang/nl-NL.json",
+    path: "./lang/nl-NL.json",
   },
 };
