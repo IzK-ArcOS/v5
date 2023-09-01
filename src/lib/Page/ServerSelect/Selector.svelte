25801eff5e10c4e28be9e392eaf5f55714ab1cdd
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-09-01 13:57:17 +0200

    Add server delete to server selector

--- src/lib/Page/ServerSelect/Selector.svelte
+++ src/lib/Page/ServerSelect/Selector.svelte
@@ -5,12 +5,14 @@
   import { getAllServers } from "../../../ts/api/server";
   import Option from "./Selector/Option.svelte";
   import Actions from "./Actions.svelte";
+  import sleep from "../../../ts/sleep";
 
   export let selected = "";
   export let servers: ServerSelectOption[] = [];
 
-  function update() {
+  async function update() {
     servers = [];
+
     const list = getAllServers().sort();
 
     for (let i = 0; i < list.length; i++) {
@@ -30,7 +32,7 @@
 <div class="selector">
   <div class="options">
     {#each servers as server}
-      <Option {server} bind:selected />
+      <Option {server} bind:selected {update} />
     {/each}
   </div>
   <Actions {selected} {servers} />
